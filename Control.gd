dc0ca58dd63d59ba3c5d49ea917f3d23c6012d3a
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2023-02-16 17:35:43 -0500

    updates

--- Control.gd
+++ Control.gd
@@ -1,23 +1,3 @@
 extends Control
 
-signal StartGame
-
-onready var _Button = $Button
-onready var _Player1_Selection = $"The Monk"
-onready var _Player2_Selection = $Princess
-func _ready():
-	_Button.connect("pressed", self, "_play_scene")
-	_Button.disabled = true
-func _play_scene():
-	get_tree().change_scene("res://BattleGrounds.tscn")
-
-func _process(delta):
-	if _Player1_Selection.pressed == true:
-		_Player1_Selection.disabled = true
-		
-	elif _Player2_Selection.pressed == true:
-		_Player2_Selection.disabled = true
-
-	elif _Player1_Selection.disabled == true and _Player2_Selection.disabled == true:
-		print("Ready")
-		_Button.disabled = false
+signal choo

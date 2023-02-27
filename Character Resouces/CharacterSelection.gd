dfd35e44fcd571e53e8bcaf6602bc334d262beae
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2023-02-27 16:11:36 -0500

    Updates

---
+++ Character Resouces/CharacterSelection.gd
@@ -0,0 +1,11 @@
+extends Node
+
+# Get character resouces to be called in character selection menu
+var SelectCharacters = {
+	"General Pyrus" : preload("res://Character Resouces/Fire Knight/Fire Knight.tscn"),
+	"Huntress" : preload("res://Character Resouces/Huntress/Huntress.tscn"),
+	"Princess Atlantis" : preload("res://Character Resouces/Princess Atlantis/Princess Atlantis.tscn"),
+	"Nomad" : preload()
+	
+	
+}

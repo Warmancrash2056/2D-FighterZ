4d5bfd7e8f56a354a91f1de442b53541ca77ece3
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2023-03-18 17:07:44 -0400

    Updates

--- Character Selection Resources/Player 2 Character Selection.gd
+++ Character Selection Resources/Player 2 Character Selection.gd
@@ -4,7 +4,7 @@ var Player2
 # Get character resouces to be called in character selection menu
 var SelectCharacters = {
 	"General Archfield" : preload("res://Character Resouces/General Archfield/Player 2 General Archfield.tscn"),
-	"Hunter" : preload("res://Character Resouces/Hunter/Player 2 Hunter.tscn"),
+	"Hunter" : preload(),
 	"Atlantis" : preload("res://Character Resouces/Princess Atlantis/Player 2 Atlantis.tscn"),
 	"Nomad" : preload("res://Character Resouces/Nomad/Nomad.tscn"),
 	"Goku" : preload("res://Character Resouces/Goku/Player 2 Goku.tscn"),

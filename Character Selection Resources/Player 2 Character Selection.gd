29fee89b6e8b57ec7ea57412af497294bdbd066d
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2023-03-18 15:42:46 -0400

    Updates

--- Character Selection Resources/Player 2 Character Selection.gd
+++ Character Selection Resources/Player 2 Character Selection.gd
@@ -3,7 +3,7 @@ extends Node
 var Player2
 # Get character resouces to be called in character selection menu
 var SelectCharacters = {
-	"General Pyrus" : preload("res://Character Resouces/General Pyrus/Player 2 General Pyrus.tscn"),
+	"General Pyrus" : preload("res://Character Resouces/General Archfield/Player 2 General Pyrus.tscn"),
 	"Hunter" : preload("res://Character Resouces/Hunter/Player 2 Hunter.tscn"),
 	"Atlantis" : preload("res://Character Resouces/Princess Atlantis/Player 2 Atlantis.tscn"),
 	"Nomad" : preload("res://Character Resouces/Nomad/Nomad.tscn"),

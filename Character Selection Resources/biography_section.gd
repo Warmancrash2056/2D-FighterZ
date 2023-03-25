c8897014fcce61af6eb2533e8ab26c60cfdc61c9
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2023-03-25 02:18:05 -0400

    update

--- Character Selection Resources/biography_section.gd
+++ Character Selection Resources/biography_section.gd
@@ -5,14 +5,11 @@ extends Control
 @onready var Biography = $Biography
 
 # Chaeracter Animation
-@onready var GeneralArchfieldAnimation = $"General Archfield"
-@onready var NiaAnimation = $"Nia Animation"
-@onready var GokuAnimation = $"Goku Animation"
-@onready var HunterAnimation = $"Hunter Animation"
-@onready var NomadAnimation = $"Nomad Animation"
-@onready var AtlantisAnimation = $"Princess Atlantis Animation"
+
+
 
 #General Archfield Abilty Showcase
+@onready var GeneralArchfieldAnimation = $"General Archfield"
 @onready var GeneralArchfieldNlightButton = $"General Archfield Nlight"
 @onready var GeneralArchfieldSlightButton = $"General Archfield Slight"
 @onready var GeneralArchfieldDlightButton = $"General Archfield Dlight"
@@ -20,11 +17,44 @@ extends Control
 @onready var GeneralArchfieldNairButton = $"General Archfield Nair"
 
 # Goku Ability Showcase
+@onready var GokuAnimation = $"Goku Animation"
 @onready var GokuNlightButton = $"Goku Nlight"
 @onready var GokuSlightButton = $"Goku Slight"
 @onready var GokuDlightButton = $"Goku Dlight"
 @onready var GokuUlightButton = $"Goku Ulight"
 @onready var GokuNairButton = $"Goku Nair"
+
+# Nomad Animation Showcas
+@onready var NomadAnimation = $"Nomad Animation"
+@onready var NomadNlightButton = $"Nomad Nlight"
+@onready var NomadSlightButton = $"Nomad Slight"
+@onready var NomadDlightButton = $"Nomad Dlight"
+@onready var NomadUlightButton = $"Nomad Ulight"
+@onready var NomadNairButton = $"Nomad Nair"
+
+# Hunter Animation Showcase
+@onready var HunterAnimation = $"Hunter Animation"
+@onready var HunterNlightButton = $"Hunter Nlight"
+@onready var HunterSlightButton = $"Hunter Slight"
+@onready var HunterDlightButton = $"Hunter Dlight"
+@onready var HunterUlightButton = $"Hunter Ulight"
+@onready var HunterNairButton = $"Hunter Nair"
+
+# Princess Atlantis Animation Showcase
+@onready var PrincessAtlantisAnimation = $"Princess Atlantis Animation"
+@onready var PrincessAtlantisNlightButton = $"Primcess Atlantis Nlight"
+@onready var PrincessAtlantisSlightButton = $"Princess Atlantis Slight"
+@onready var PrincessAtlantisDlightButton = $"Princess Atlantis Dlight"
+@onready var PrincessAtlantisUlightButton= $"Princess Atlantis Ulight"
+@onready var PrincessAtlantisNairButton = $"Princess Atlantis Nair"
+
+# Nia Animation Animation Showcase
+@onready var NiaAnimation = $"Nia Animation"
+@onready var NiaNlight = $"Nia Nlight"
+@onready var NiaSlight = $"Nia Slight"
+@onready var NiaDlight = $"Nia Dlight"
+@onready var NiaUlight = $"Nia Ulight"
+@onready var NiaNair = $"Nia Nair"
 
 enum Characters {
 	Set,
@@ -79,16 +109,51 @@ func _ready():
 func _process(delta):
 	match CharacterState:
 		Characters.Set:
+			# Disiable Animations when process begins.
+			GeneralArchfieldAnimation.visible = false
+			PrincessAtlantisAnimation.visible = false
+			NiaAnimation.visible = false
+			GokuAnimation.visible = false
+			NomadAnimation.visible = false
+			HunterAnimation.visible = false
+			
+			
 			GeneralArchfieldNlightButton.disabled = true
 			GeneralArchfieldSlightButton.disabled = true
 			GeneralArchfieldDlightButton.disabled = true
 			GeneralArchFieldUlightButton.disabled = true
 			GeneralArchfieldNairButton.disabled = true
+			
 			GokuNlightButton.disabled = true
 			GokuSlightButton.disabled = true
 			GokuDlightButton.disabled = true
 			GokuUlightButton.disabled = true
-			GokuNairButton.disabled = true	
+			GokuNairButton.disabled = true
+			
+			HunterNlightButton.disabled = true
+			HunterSlightButton.disabled = true
+			HunterDlightButton.disabled = true
+			HunterUlightButton.disabled = true
+			HunterNairButton.disabled = true
+			
+			NiaNlight.disabled = true
+			NiaSlight.disabled = true
+			NiaDlight.disabled = true
+			NiaUlight.disabled = true
+			NiaNair.disabled = true
+			
+			NomadNlightButton.disabled = true
+			NomadSlightButton.disabled = true
+			NomadDlightButton.disabled = true
+			NomadUlightButton.disabled = true
+			NomadNairButton.disabled = true
+			
+			PrincessAtlantisNlightButton.disabled = true
+			PrincessAtlantisSlightButton.disabled = true
+			PrincessAtlantisDlightButton.disabled = true
+			PrincessAtlantisUlightButton.disabled = true
+			PrincessAtlantisNairButton.disabled = true
+			
 		Characters.GeneralArchfieldIdle:
 			GeneralArchfieldAnimation.visible = true
 			AboutMe.set_text(str("General Arcfield is a heavy character with slow speed but the strongest character in the game with high hp.
@@ -111,23 +176,125 @@ func _process(delta):
 			GeneralArchfieldAnimation.play("Ulight")
 		Characters.GeneralArchfieldNair:
 			GeneralArchfieldAnimation.play("Nair")
+		Characters.GokuIdle:
+			GokuNlightButton.visible = false
+			GokuAnimation.play("Idle")
+		Characters.GokuNlight:
+			GokuAnimation.play("Nlight")
+		Characters.GokuSlight:
+			GokuAnimation.play("Slight")
+		Characters.GokuDlight:
+			GokuAnimation.play("Dlight")
+		Characters.GokuUlight:
+			GokuAnimation.play("Ulight")
+		Characters.GokuNair:
+			GokuAnimation.play("Nair")
+		Characters.NomadIdle:
+			NomadAnimation.play("Idle")
+		Characters.NomadNlight:
+			NomadAnimation.play("Nlight")
+		
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Character Selection Resources/Character Selection Menu.tscn")
 
 
 func _on_general_archfield_nlight_pressed():
 	CharacterState = Characters.GeneralAarchfiedNlight
-
 func _on_general_archfield_slight_pressed():
 	CharacterState = Characters.GeneralArchfieldSlight
 func _on_general_archfield_dlight_pressed():
 	CharacterState = Characters.GeneralArchfieldDlight
 func _on_general_archfield_ulight_pressed():
 	CharacterState = Characters.GeneralArchfieldUlight
-
-
 func _on_general_archfield_nair_pressed():
 	CharacterState = Characters.GeneralArchfieldNair
+func _on_goku_nlight_pressed():
+	CharacterState = Characters.GokuNlight
+func _on_goku_slight_pressed():
+	CharacterState = Characters.GokuSlight
+func _on_goku_dlight_pressed():
+	CharacterState = Characters.GokuDlight
+func _on_goku_ulight_pressed():
+	CharacterState = Characters.GokuNlight
+func _on_goku_nair_pressed():
+	CharacterState = Characters.GokuNair
+func _on_nomad_nlight_pressed():
+	CharacterState = Characters.NomadNlight
+func _on_nomad_slight_pressed():
+	CharacterState = Characters.NomadNlight
+func _on_nomad_dlight_pressed():
+	pass # Replace with function body.
+
+
+func _on_nomad_ulight_pressed():
+	pass # Replace with function body.
+
+
+func _on_nomad_nair_pressed():
+	pass # Replace with function body.
+
+
+func _on_hunter_nlight_pressed():
+	pass # Replace with function body.
+
+
+func _on_hunter_slight_pressed():
+	pass # Replace with function body.
+
+
+func _on_hunter_dlight_pressed():
+	pass # Replace with function body.
+
+
+func _on_hunter_ulight_pressed():
+	pass # Replace with function body.
+
+
+func _on_hunter_nair_pressed():
+	pass # Replace with function body.
+
+
+func _on_primcess_atlantis_nlight_pressed():
+	pass # Replace with function body.
+
+
+func _on_princess_atlantis_slight_pressed():
+	pass # Replace with function body.
+
+
+func _on_princess_atlantis_dlight_pressed():
+	pass # Replace with function body.
+
+
+func _on_princess_atlantis_ulight_pressed():
+	pass # Replace with function body.
+
+
+func _on_princess_atlantis_nair_pressed():
+	pass # Replace with function body.
+
+
+func _on_nia_nlight_pressed():
+	pass # Replace with function body.
+
+
+func _on_nia_slight_pressed():
+	pass # Replace with function body.
+
+
+func _on_nia_dlight_pressed():
+	pass # Replace with function body.
+
+
+func _on_nia_ulight_pressed():
+	pass # Replace with function body.
+
+
+func _on_nia_nair_pressed():
+	pass # Replace with function body.
+
+	
+		
 func _on_general_archfield_detector_area_entered(area):
 	if area:
 		CharacterState = Characters.GeneralArchfieldIdle
@@ -207,3 +374,4 @@ func _on_princess_atlantis_detector_area_entered(area):
 
 func _on_princess_atlantis_detector_area_exited(area):
 	pass # Replace with function body.
+

c5e0a05e08a1548401939e02ba654bb5b744c51a
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2023-04-06 18:31:03 -0400

    Updates

--- Autoloads/Player 2 Character Selection.gd
+++ Autoloads/Player 2 Character Selection.gd
@@ -7,6 +7,6 @@ var SelectCharacters = {
 	"Hunter" : preload("res://Character Resouces/Hunter/Character Scene/Player 2 Hunter.tscn"),
 	"Princess Atlantis" : preload("res://Character Resouces/Princess Atlantis/Character Scenes/Player 2 Princess Atlantis.tscn"),
 	"Goku" : preload("res://Character Resouces/Goku/Character Scenes/Player 2 Goku.tscn"),
-	"Nomad" : preload("res://Character Resouces/Nomad/Character Scenes/Player 2  Nomad.tscn"),
+	"Nomad" : preload("res://Character Resouces/Nomad/Character Scenes/Player 2 Nomad.tscn"),
 	"Nia" : preload("res://Character Resouces/Nia/Character Scene/Player 2 Nia.tscn")
 }

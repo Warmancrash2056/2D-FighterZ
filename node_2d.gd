b779990c69429f4f065bf209debea2bbacefbd42
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2023-04-26 12:38:31 -0400

    Added New Logo

---
+++ node_2d.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("Bing")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "Bing":
+		$AnimationPlayer.play("Bing")

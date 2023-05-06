6c4c08cea4fcbe54e43b16a377f8020266dbf670
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2023-05-06 02:06:03 -0400

    Updates

--- Game Start/Game Audio.gd
+++ Game Start/Game Audio.gd
@@ -4,12 +4,30 @@ extends AudioStreamPlayer2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	play()
+	volume_db = 10
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if $CheckButton.button_pressed == true:
+		stream_paused = true
+		
+	else:
+		stream_paused = false
 
 
 func _on_finished():
 	play()
+
+
+
+func _on_volume_down_pressed():
+	volume_db -= 2
+
+	if volume_db >= 20:
+		volume_db = 20
+func _on_volume_up_pressed():
+	volume_db += 2
+	
+	if volume_db == 0:
+		volume_db = 0

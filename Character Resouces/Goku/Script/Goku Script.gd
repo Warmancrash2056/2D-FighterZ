6e734b241892fbcaacf6ffd05ab840c035206b50
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2023-05-08 19:27:53 -0400

    Updates

--- Character Resouces/Goku/Script/Goku Script.gd
+++ Character Resouces/Goku/Script/Goku Script.gd
@@ -6,248 +6,166 @@ extends CharacterBody2D
 @onready var Animate = $"Scale Player/AnimationPlayer"
 @onready var SpriteH = $Animation
 
-# Action Bar Nodes #
-@onready var ActionBar = $"Status Bar/Action Bar"
-@onready var Healthbar = $"Status Bar/Health Bar"
-@onready var ActionNotifier = $"Status Bar/Action Notifier"
-@onready var HealthNotifier = $"Status Bar/Health Notifier"
-@onready var PointsPlayer = $"Add Points"
-@onready var ActionBrokenPlayer = $"Status Bar/Action Break"
-
-@export var Movement: int  = 300
-@export var AirMovement: int  = 150
-@export var Acceleration: int  = 35
-@export var JumpHeight: int = 500
-@export var Gravity : int  = 5
+@export var Movement: int
+@export var AirMovement: int
+@export var JumpHeight: int
+@export var Gravity : int
 
 @export var Health: int
-@export var ActionPts: int
 
-var Motion = Vector2.ZERO
 var Up = Vector2.UP
 var Can_Attack = true
 var Action_Exceeded = false
 var Jump_Count = 3
-var Direction = 1
-enum States { Idle, Jump, Fall, Nlight, Slight, Dlight, Ulight,
-	Nair,
-	Defend,
-	Roll,
-	Death,
-	Hurt
-}
+enum States {Idle, Jump, Fall, Nlight, Slight, Dlight, Ulight, Nair, Defend, Roll, Death, Hurt}
 var Select = States.Idle
 
 func _ready():
-	
-	# Set Status bar when player loaded #
-	ActionBrokenPlayer.play("Normal")
+	pass
 
-func _stop_points():
-	PointsPlayer.stop()
 
-func _start_points():
-	PointsPlayer.play("Add Points")
-# Checks if action points needs to be replenished if under 6 points.
-func _add_action_pts():
-	
-	# Add 1pt of action after idle or run animation is finished. 
-	if ActionPts < 9:
-		ActionPts += 1
-		
-	
-	# If action points are at 6. Set action points to stop increasing number.
-	else:
-		ActionPts = 9
 func _process(delta):
-	PointsPlayer.play("Add Points")
-	ActionBar.value = ActionPts
-	ActionBar.max_value = 9
-	Healthbar.value = Health
-	Healthbar.max_value = 500
-	ActionNotifier.set_text(str(ActionPts))
-	HealthNotifier.set_text(str(Health))
-
-	if Motion.x >= 1:
+	if velocity.x >= 1:
 		SpriteH.flip_h = false
 		$"Scale Player".set_scale(Vector2(abs($"Scale Player".get_scale().x), $"Scale Player".get_scale().y))
-	elif Motion.x <= -1:
+	elif velocity.x <= -1:
 		SpriteH.flip_h = true
-		$"Scale Player".set_scale(Vector2(-abs($"Scale Player".get_scale().x), $"Scale Player".get_scale().y))
-	
-	if ActionPts <= 0:
-		Can_Attack = false
-	else:
-		Can_Attack = true
-	
-	if ActionPts >= 1:
-		ActionBar.texture_under = load("res://Health System/Unbroken Progress Background.png")
-		Action_Exceeded = false
-	if ActionPts <= -1:
-		ActionBar.texture_under = load("res://Health System/Broken Progress Background.png")
-		ActionBrokenPlayer.play("Actions Exceeded")
-		Action_Exceeded = true
 		
 func _physics_process(delta):
-	PointsPlayer.play("Add Points")
-	set_velocity(Motion)
-	set_up_direction(Up)
+	velocity.y += Gravity
 	move_and_slide()
-	Motion = velocity
 
 	match Select:
 
 		States.Idle:
 			Jump_Count = 3
-			Motion.y += Gravity * delta
-				
 				
 			if Input.is_action_pressed(controls.input_left):
 				Animate.play("Run")
-				Motion.x = max(Motion.x - Acceleration, -Movement)
-				if Can_Attack == true:
-					if Input.is_action_just_pressed(controls.input_attack):
+				velocity.x = -Movement
+				if Input.is_action_just_pressed(controls.input_attack):
 						Select = States.Slight
-						ActionPts -= 2
 						
-					if Input.is_action_just_pressed(controls.input_dash):
+				if Input.is_action_just_pressed(controls.input_dash):
 						Select = States.Roll
-						Motion.x = -350
-						ActionPts -= 6
+						velocity.x = -350
 			elif Input.is_action_pressed(controls.input_right):
 				Animate.play("Run")
-				Motion.x = min(Motion.x + Acceleration, Movement)
+				velocity.x = Movement
 				
-				if Can_Attack == true:
-					if Input.is_action_just_pressed(controls.input_attack):
-						Select = States.Slight
-						ActionPts -= 2
+				if Input.is_action_just_pressed(controls.input_attack):
+					Select = States.Slight
 						
 						
-					if Input.is_action_just_pressed(controls.input_dash):
-						Select = States.Roll
-						Motion.x = 350
-						ActionPts -= 6
+				if Input.is_action_just_pressed(controls.input_dash):
+					Select = States.Roll
+					velocity.x = 350
 			
 			elif Input.is_action_pressed(controls.input_down):
 				Animate.play("Idle")
 				if Can_Attack == true:
 					if Input.is_action_just_pressed(controls.input_attack):
 						Select = States.Dlight
-						ActionPts -= 5
 
 					
 			elif Input.is_action_pressed(controls.input_up):
 				Animate.play("Idle")
 				if Input.is_action_just_pressed(controls.input_attack):
 					Select = States.Ulight
-					ActionPts -= 7
 			else:
-				Motion.x = lerp(Motion.x , 0.01, 0.8)
+				velocity.x = 0
 				Animate.play("Idle")
 				
 				if Input.is_action_just_pressed(controls.input_attack):
 					Select = States.Nlight
-					ActionPts -= 1
 				elif Input.is_action_just_pressed(controls.input_block):
 					Select = States.Defend
-					ActionPts -= 8
 			if Action_Exceeded == false:
 				if Input.is_action_just_pressed(controls.input_jump) and Jump_Count > 0:
 					Animate.play("Jump")
 					Select = States.Jump
 					Jump_Count -= 1
 					$"Jump Sound".play()
-					Motion.y = -JumpHeight
+					velocity.y = -JumpHeight
 				
 		States.Jump:
-			Motion.y += Gravity
 			if is_on_floor():
 				Select = States.Idle
-					
 			if Input.is_action_pressed(controls.input_down):
-				Motion.y += 1
+				velocity.y += 1
 				Animate.play("Fall")
-				
-			if Input.is_action_just_pressed(controls.input_jump) and Jump_Count > 0:
-				Motion.y = -JumpHeight
-				Jump_Count -= 1
-				Animate.play("Jump")
-				$"Jump Sound".play()
+			if not is_on_floor():
+				if Input.is_action_just_pressed(controls.input_jump) and Jump_Count > 0:
+					velocity.y = -JumpHeight
+					Jump_Count -= 1
+					$"Jump Sound".play()
+					Animate.play("Jump")
 				
 			if Input.is_action_pressed(controls.input_left):
-				Motion.x = max(Motion.x - Acceleration, -AirMovement)
+				velocity.x = -AirMovement
 				
 				
 				if Input.is_action_just_pressed(controls.input_attack):
 					Select = States.Nair
 				
 			elif Input.is_action_pressed(controls.input_right):
-				Motion.x = min(Motion.x + Acceleration, AirMovement)
+				velocity.x = AirMovement
 				
 				if Input.is_action_just_pressed(controls.input_attack):
 					Select = States.Nair
 			else:
-				Motion.x = lerp(Motion.x , 0.01, 0.01)
+				velocity.x = 0
 				
 				if Input.is_action_just_pressed(controls.input_attack):
 					Select = States.Nair
-					ActionPts -= 4
 
 		States.Nlight:
-			Motion.x = 0
+			velocity.x = 0
 			Animate.play("Nlight")
 			
 		States.Slight:
-			Motion.x = 0
-			Motion.y = 0
+			velocity.x = 0
+			velocity.y = 0
 			
 			Animate.play("Slight")
 
 		States.Dlight:
-			Motion.x = 0
-			Motion.y = 0
+			velocity.x = 0
+			velocity.y = 0
 			Animate.play("Dlight")
 			
 				
 		States.Ulight:
-			Motion.x = 0
-			Motion.y = 0
+			velocity.x = 0
+			velocity.y = 0
 			Animate.play("Ulight")
 			
 				
 		States.Nair:
-			Motion.x = lerp(Motion.x , 0.1, 0.03)
-			Motion.y = 0
+			velocity.x = lerp(velocity.x , 0.1, 0.03)
+			velocity.y = 0
 			Animate.play("Nair")
 
 			
 		States.Defend:
 			Animate.play("Block")
-			Motion.y = 0
-			Motion.x = 0
+			velocity.y = 0
+			velocity.x = 0
 			
 		
 			
 			
 		States.Roll:
-			Motion.x = lerp(Motion.x , 0.01, 0.02)
-			Motion.y += Gravity
+			velocity.x = lerp(velocity.x , 0.01, 0.02)
 			Animate.play("Roll")
 			if !is_on_floor():
 				Select = States.Fall
-			await get_tree().create_timer(0.05).timeout
-			if Input.is_action_just_pressed(controls.input_block):
-				Select = States.Defend
-				
-			elif Input.is_action_just_pressed(controls.input_jump):
-				Select = States.Jump
 				
 		States.Death:
 			Animate.play("Jump")
 			
 		States.Hurt:
-			Motion.x = 0
+			velocity.x = 0
 			Animate.play("Take Hit")
 
 

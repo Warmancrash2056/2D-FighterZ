daec42a2e6aae6de1b95a3df3171fd814ae08e3b
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2023-05-19 20:04:56 -0400

    Updates

--- Exit Game Prompt.gd
+++ Exit Game Prompt.gd
@@ -3,7 +3,58 @@ extends Control
 @onready var leave_game = $"Leave Game"
 @onready var return_game = $"Return to menu"
 
+@onready var GeneralArchfield = $"../General Archfield"
+@onready var Goku = $"../Goku"
+@onready var Nomad = $"../Nomad"
+@onready var Nia = $"../Nia"
+@onready var PrincessAtlantis = $"../Princess Atlantis"
+@onready var Hunter = $"../Hunter"
 
+enum Game{
+	Escape,
+	Return,
+	
+}
+var CheckGame = Game.Return
+func _ready():
+	visible = false
+		
+	GeneralArchfield.play("Idle")
+	Goku.play("Idle")
+	Hunter.play("Idle")
+	Nomad.play("Idle")
+	PrincessAtlantis.play("Idle")
+	Nia.play("Idle")
+func _process(delta):
+	
+	match CheckGame:
+		Game.Return:
+			visible = false
+			if Input.is_action_just_pressed("exit"):
+				CheckGame = Game.Escape
+				
+			GeneralArchfield.play("Idle")
+			Goku.play("Idle")
+			Hunter.play("Idle")
+			Nomad.play("Idle")
+			PrincessAtlantis.play("Idle")
+			Nia.play("Idle")
+		Game.Escape:
+			visible = true
+			if Input.is_action_just_pressed("exit"):
+				CheckGame = Game.Return
+				
+				
+			GeneralArchfield.stop()
+			Goku.stop()
+			Hunter.stop()
+			Nomad.stop()
+			PrincessAtlantis.stop()
+			Nia.stop()
 
 func _on_leave_game_pressed():
 	get_tree().quit()
+
+
+func _on_return_to_menu_pressed():
+	CheckGame = Game.Return
--- Game Start/Start Game.gd
+++ Game Start/Start Game.gd
@@ -12,55 +12,9 @@ extends Control
 @onready var AboutCharacters = $"About Characters"
 @onready var TrainingRoom = $"Training Room"
 
-@onready var EscapeBackground = $"End Game/ColorRect"
-@onready var EscapeText = $"End Game/RichTextLabel"
-@onready var TimerText = $"End Game/Timer"
-@onready var EscapeTimer = $Timer
+@onready var exit_prompt = $"Exit Game Prompt"
 
-var set_time = false
-enum Game{
-	EndGame,
-	Escape,
-	Return,
-	
-}
-var CheckGame = Game.Return
-func _ready():
-	pass
-func _process(delta):
-	
-	match CheckGame:
-		Game.Return:
-			EscapeBackground.visible = false
-			EscapeText.visible = false
-			TimerText.visible = false
-			if Input.is_action_just_pressed("exit"):
-				CheckGame = Game.Escape
-				EscapeTimer.start()
-			GeneralArchfield.play("Idle")
-			Goku.play("Idle")
-			Hunter.play("Idle")
-			Nomad.play("Idle")
-			PrincessAtlantis.play("Idle")
-			Nia.play("Idle")
-		Game.Escape:
-			TimerText.set_text(str(int(EscapeTimer.time_left),"s"))
-			EscapeBackground.visible = true
-			EscapeText.visible = true
-			TimerText.visible = true
-			if Input.is_action_just_pressed("exit"):
-				get_tree().quit()
-				
-			GeneralArchfield.stop()
-			Goku.stop()
-			Hunter.stop()
-			Nomad.stop()
-			PrincessAtlantis.stop()
-			Nia.stop()
-			
-		Game.EndGame:
-			pass
-			
+
 
 func _on_local_play_pressed():
 	get_tree().change_scene_to_file("res://Game Start/Local Play/Local Play.tscn")
@@ -72,8 +26,6 @@ func _on_about_characters_pressed():
 
 func _on_training_room_pressed():
 	get_tree().change_scene_to_file("res://Game Maps/Training'/Training Character Selection.tscn")
-func _on_timer_timeout():
-	CheckGame = Game.Return
 
 
 func _on_button_pressed():

18230dc492a6930cf5e3f5b1d57561cdf4d9ce41
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2023-08-11 11:40:05 -0400

    Update Game Audio.gd
    Updated to better explain its purpose.

--- Autoloads/Game Audio.gd
+++ Autoloads/Game Audio.gd
@@ -1,18 +1,12 @@
 extends AudioStreamPlayer2D
 
+# Load audio sound.
 var Main_Menu_Audio = load("res://Game Music/Ludum Dare 38 - Track 2.wav")
 var Galvin_Map_Audio = load("res://Game Music/Galvin.wav")
 var Artic_Map_Audio = load("res://Game Music/The Artic.wav")
 var Character_Selection_Audio = load("res://Game Music/Ludum Dare 38 - Track 3.wav")
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+# Play audio when selecred by scene script on _ready(). 
 func _main_menu_play():
 	stream = Main_Menu_Audio
 	play()
@@ -29,5 +23,6 @@ func _character_select_play():
 	stream = Character_Selection_Audio
 	play()
 
+# Repeat Audio after finish.
 func _on_finished():
 	play()

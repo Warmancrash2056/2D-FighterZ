0a873136d9cff1c9880e31fd13a181b4e591c3e9
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2023-08-11 11:35:56 -0400

    Update Character List.gd
    Commented on character list to better explain the variables amd purpose.

--- Autoloads/Character List.gd
+++ Autoloads/Character List.gd
@@ -1,18 +1,24 @@
 extends Node
 
+# Get the player 
 var get_player_1
 var get_player_2
 var get_main_player
 
+# If player is called set true to move to map selction.
 var check_player_1_is_called = false
 var check_player_2_is_called = false
 var check_main_is_called = false
 
+# Check if player is facing left for each player script. 
 var player_1_facing_left = false
 var player_2_facing_left= false
 var main_player_facing_left = false
 
+# Disable all small platforms. 
+# Hunter has the ability to root a platform to prevent the opponent from climbing this does not effect the user. 
 var disable_mini_platform = false
+
 # Reminder that if script is not erroing and cannot register script. 
 # Usually means that script has an error.
 var get_player_1_script = preload("res://Character Resouces/Global/Player 1 Script.gd")

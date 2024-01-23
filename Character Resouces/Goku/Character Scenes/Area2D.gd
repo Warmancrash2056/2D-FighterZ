7769dd3b0b1796df642fa8386d35cd2483583739
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2024-01-23 08:36:50 -0500

    Updates

---
+++ Character Resouces/Goku/Character Scenes/Area2D.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@export var Knockback_X: int 
+@export var Knockback_Y: int
+# Called when the node enters the scene tree for the first time.
+func _ready():
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass

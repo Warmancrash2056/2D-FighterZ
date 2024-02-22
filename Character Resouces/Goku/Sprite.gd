e4fa38e931aeacfb8df95ac2c85e9c2137ad1669
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2024-02-21 21:32:30 -0500

    General Updates

---
+++ Character Resouces/Goku/Sprite.gd
@@ -0,0 +1,19 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	print(flip_h)
+
+
+func _on_character_facing_left():
+	flip_h = true
+
+
+func _on_character_facing_right():
+	flip_h = false

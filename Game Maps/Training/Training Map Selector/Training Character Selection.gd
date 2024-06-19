101f8e33e7f8c24c4ea71d91c66ab2080f8218f8
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2024-06-18 21:41:01 -0400

    Revert "Updates"
    This reverts commit ff4198160ce4ca6f9ee2c2b4e2e749970b1bacd6.

--- Game Maps/Training/Training Map Selector/Training Character Selection.gd
+++ Game Maps/Training/Training Map Selector/Training Character Selection.gd
@@ -5,7 +5,7 @@ extends Control
 var main_controller: Resource = preload("res://Character Resouces/Global/Controller Resource/Player_3.tres")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	GameAuido._character_select_play()
+	Audio._character_select_play()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Player1.Player1Ready == true:
@@ -17,7 +17,7 @@ func _process(delta):
 		$"Prompt Name".set_text(str("Select Characters"))
 	if Input.is_action_just_pressed("exit"):
 		get_tree().change_scene_to_file("res://Game Start/Main Menu/Main Menu.tscn")
-		GameAuido._main_menu_play()
+		Audio._main_menu_play()
 		Player1.Player1Ready = false
 		CharacterList.get_main_player = null
 

e8ef66797b66893441509cdc4beec36222731ff8
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2024-06-24 12:13:17 -0400

    Update About Character.gd

--- Game Start/Aboou Character/About Character.gd
+++ Game Start/Aboou Character/About Character.gd
@@ -59,11 +59,12 @@ extends Node2D
 @onready var NiaNairButton = $"Nia Character Selection/Nia Nair"
 
 func _ready():
-	Audio.Main_Menu_Audio
+	GameAuido.Main_Menu_Audio
 func _physics_process(delta):
 	if Input.is_action_just_pressed("exit"):
 		get_tree().change_scene_to_file("res://Game Start/Main Menu/Main Menu.tscn")
-		Audio.Main_Menu_Audio
+		GameAuido.Main_Menu_Audio
+
 func _on_general_archfield_animation_looped():
 	GeneralArchfieldAnimation.play("Idle")
 

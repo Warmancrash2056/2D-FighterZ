03303ddd21b91b4283f34807a34d920f3e5e29fa
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2024-06-24 12:11:50 -0400

    Create Main Healthbar.gd

---
+++ Ui Resources/Main Healthbar.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$ProgressBar.value = CharacterList.player_1_health
+	$Sprite2D.texture = CharacterList.icon
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$ProgressBar.value = CharacterList.health
+
+
+	if CharacterList.goku_selected == true:
+		$Sprite2D.texture = load("res://Character Resouces/Goku/Icon/Goku.png")
+
+	if CharacterList.sakura_selected == true:
+		$prite.texture = load("res://Character Resouces/Sakura/Icon/Assasin Character Icon.png")

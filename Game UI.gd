8b0ccacc1a102602b0f94d2a7345bb98c850838f
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2024-07-17 09:35:55 -0400

    Changes to ui

---
+++ Game UI.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+@onready var Match_Time: Timer = $Timer
+@onready var Match_Text: RichTextLabel = $RichTextLabel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Match_Time.start()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	Match_Text.set_text(str(int(Match_Time.time_left)))

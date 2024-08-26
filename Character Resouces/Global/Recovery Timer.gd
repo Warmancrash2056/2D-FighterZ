39368cb23847c5f62f344d571d67072940167917
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2024-08-26 18:52:18 -0400

    Update Recovery Timer.gd

--- Character Resouces/Global/Recovery Timer.gd
+++ Character Resouces/Global/Recovery Timer.gd
@@ -23,7 +23,7 @@ func calculate_recovery(new_recovery: float):
 	Movement_Cooldown.set_wait_time(movement_cooldown_timer)
 	set_wait_time(new_recovery)
 	#print(wait_time)
-	print("Reduced Attack Cooldoen " ,wait_time,"Reduced Movement Cooldoen " ,movement_cooldown_timer)
+	#print("Reduced Attack Cooldoen " ,wait_time,"Reduced Movement Cooldoen " ,movement_cooldown_timer)
 
 
 func _on_attack_connected() -> void:
@@ -32,7 +32,7 @@ func _on_attack_connected() -> void:
 	set_wait_time(reduced_recovery)
 	Movement_Cooldown.set_wait_time(0.01)
 
-	print("Hit Registered on Attack Cooldoen " ,wait_time,"Reduced Movement Cooldoen " ,Movement_Cooldown.wait_time)
+	#print("Hit Registered on Attack Cooldoen " ,wait_time,"Reduced Movement Cooldoen " ,Movement_Cooldown.wait_time)
 
 
 func _on_side_air__2_attack_connected() -> void:

663fc3494b75f4f427c618374dde234e6cd54e9f
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2024-09-02 15:14:39 -0400

    Update Player Stats.gd

--- Character Resouces/Global/Player Stats.gd
+++ Character Resouces/Global/Player Stats.gd
@@ -7,9 +7,9 @@ extends Node
 @export var Jump_Height: int
 @export var Jump_Count:int = 3
 @export var Gravity: int
-@export var Defense_Rating: float # Decreases the amount of damage taken per incoming attack
+@export var Defense_Rating: int ## Reduces the amount of Constant Knockback
 @export var Health: float
-@export var Attack_Rating: int # Increases base damage of an outgoing attack
+@export var Attack_Rating: int # Increses the knockback of the variable force
 @export var Stamina_Rating: float
 # Decreases the amount of recovery frames you are in before you can attack or move
 # Also effects the your stun time from an incoming attack

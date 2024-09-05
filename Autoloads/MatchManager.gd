3e928a574bb4baa75c95d48a677bb6b81b61bc9a
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2024-09-04 20:04:35 -0400

    Create MatchManager.gd

---
+++ Autoloads/MatchManager.gd
@@ -0,0 +1,6 @@
+## Manages the match.
+class_name MatchManager extends Node
+
+# Relays the position of the players from the player setter configuration.
+var player_1_global_position: Vector2
+var player_2_global_position: Vector2

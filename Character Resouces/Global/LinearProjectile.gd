fd562dd01bd9278d63ceadd3f6b87940d763cc3e
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2024-11-25 04:06:58 -0500

    Fixed some buggy code
    Projectile was flipping sprite Constantly
    Added a match statement to switch between satates

--- Character Resouces/Global/LinearProjectile.gd
+++ Character Resouces/Global/LinearProjectile.gd
@@ -9,13 +9,17 @@ var collider_mask: int
 @export var Animator: AnimationPlayer
 @export var Sprite: Sprite2D
 
+enum {Shoot, Hit}
+var state = Shoot
 func _ready():
 	if direction == 1:
 		Sprite.flip_h = false
 
 	else:
+		if direction == -1:
+			Sprite.flip_h = true
 		Sprite.flip_h = true
-	Animator.play("Shoot")
+	state = Shoot
 
 
 
@@ -26,23 +30,16 @@ func _process(delta):
 	Projectile_Hitbox.set_collision_layer_value(Projectile_Layer,true)
 	Projectile_Hitbox.set_collision_mask_value(Projectile_Mask, true)
 	move_and_slide()
-	if direction == 1:
-		Sprite.flip_h = false
-
-	else:
-		Sprite.flip_h = true
-	velocity.x = move_toward(velocity.x, direction * Max_Speed, Acceleration)
-	if direction == 1:
-		scale.x = 1
-
-	else:
-		scale.x = -1
+	match state:
+		Shoot:
+			Animator.play("Shoot")
+			velocity.x = move_toward(velocity.x, direction * Max_Speed, Acceleration)
+		Hit:
+			Animator.play("Hit")
+			velocity = Vector2.ZERO
 
 func _on_area_2d_body_entered(body):
-	Animator.play("Hit")
-	velocity = Vector2.ZERO
-
+	state = Hit
 
 func _on_area_2d_area_entered(area):
-	Animator.play("Hit")
-	velocity = Vector2.ZERO
+	state = Hit

d311a42e401b7107f63e4b75f84946833b1a42d2
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2024-11-28 03:19:07 -0500

    Update Projectile Position.gd

--- Character Resouces/Global/Projectile Position.gd
+++ Character Resouces/Global/Projectile Position.gd
@@ -4,31 +4,25 @@ extends Marker2D
 @export var Air_Projectle: Resource
 @export var Ground_Projectile: Resource
 @export var Sprite: Sprite2D
-func _create_projectile():
-	print(Player_Identifier.Attack_Layer)
-	print(Player_Identifier.Attack_Mask)
+
+func _Ground_Projectile():
 	var instance_ground_projectile = Ground_Projectile.instantiate()
-	var instance_air_projectile = Air_Projectle.instantiate()
-
-	# Set the collider values before instancing projectile.
-	instance_air_projectile.global_position = global_position
-	instance_ground_projectile.global_position = global_position
-	# Change the root node scaling based on player sprite facing.
-	instance_air_projectile.Projectile_Layer = Player_Identifier.Attack_Layer
-	instance_air_projectile.Projectile_Mask = Player_Identifier.Attack_Mask
-	instance_ground_projectile.Projectile_Layer = Player_Identifier.Attack_Layer
-	instance_ground_projectile.Projectile_Mask = Player_Identifier.Attack_Mask
-
 	if Sprite.flip_h == false:
 		instance_ground_projectile.direction = 1
-		instance_air_projectile.direction = 1
 	else:
 		instance_ground_projectile.direction = -1
-		instance_air_projectile.direction = -1
+	instance_ground_projectile.global_position = global_position
+	instance_ground_projectile.Projectile_Layer = Player_Identifier.Attack_Layer
+	instance_ground_projectile.Projectile_Mask = Player_Identifier.Attack_Mask
+	get_tree().get_root().add_child(instance_ground_projectile)
 
-	if Controller.is_on_floor():
-		get_tree().get_root().add_child(instance_ground_projectile)
-
+func _Air_Projectile():
+	var instance_air_projectile = Air_Projectle.instantiate()
+	if Sprite.flip_h == false:
+		instance_air_projectile.direction = 1
 	else:
-		if !Controller.is_on_floor():
-			get_tree().get_root().add_child(instance_air_projectile)
+		instance_air_projectile.direction = -1
+	instance_air_projectile.global_position = global_position
+	instance_air_projectile.Projectile_Layer = Player_Identifier.Attack_Layer
+	instance_air_projectile.Projectile_Mask = Player_Identifier.Attack_Mask
+	get_tree().get_root().add_child(instance_air_projectile)

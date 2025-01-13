5dea3b9cae8e01a0b9eba50e2a4b6f7023bb687f
Davaughn C James <68933308+Warmancrash2056@users.noreply.github.com>
2025-01-13 18:00:51 -0500

    Updates

--- Game Maps/Galvin Maps/Map Scenes/area_2d.gd
+++ Game Maps/Galvin Maps/Map Scenes/area_2d.gd
@@ -1,12 +1,19 @@
 extends Area2D
 
 @export var Collider: CollisionShape2D
+@export var Reset_Timer: Timer
+var respawn_space_occupied = false ## Checks if area is cleared for player to respawn
+var respawn_position: Vector2
+func _ready() -> void:
+	respawn_position = Collider.global_position
 
-func  _physics_process(delta: float) -> void:
-	if has_overlapping_bodies():
-		Collider.debug_color = Color.RED
-		Collider.debug_color = Color.YELLOW
-		Collider.debug_color = Color.GREEN
 
-	else:
-		Collider.debug_color = Color.BLUE
+
+func _on_body_entered(body: Node2D) -> void:
+	respawn_space_occupied = true
+	Collider.disabled = true
+	Reset_Timer.start()
+
+func _on_timer_timeout() -> void:
+	respawn_space_occupied = false
+	Collider.disabled = false
